32ff42ede573b8bda3990b9f5deb6d089e68a779
Federico Ramos <federamos2203@gmail.com>
2024-06-25 21:20:24 -0300

    intro a Variables

---
+++ 04-introjavascript/matematic.js
@@ -0,0 +1,15 @@
+
+// operadores mátematicos
+// + - * / %
+let numUno = 15;
+let numDos = 50;
+
+let resultado = numUno + numDos;
+
+console.log(numUno + numDos); 
+
+console.log(20 + 50);
+
+console.log(resultado);
+
+console.log("El resultado de la suma es:", resultado);
---
+++ 04-introjavascript/variables.js
@@ -0,0 +1,87 @@
+// estos son los comentarios  de una sola linea en js
+
+/* 
+
+Comentarios
+de
+varias
+lineas
+en
+js
+
+*/
+
+//let hola = 'Pepe'
+
+/*  
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+let hola = 'Pepe'
+*/
+
+//variables: espacios de memoria para almacenar datos
+//1. con var
+// Palabras reservadas son utilizadas solos para las definiciones del lenguaje de programacion 
+var nombre; // caracters/letras
+var email; // números
+var edad; 
+var pais;
+
+//no
+/*
+    var 1234;
+    var nombre apellido;
+    var =pais;
+    var -edad;
+*/
+
+//si
+var $provincia;
+var _nombre;
+var nombreApellidoPersona; //camelcase
+var nombre_apellido; //undercase
+
+let provincia;
+
+//const representa una constante: no cambia enla ejecución del programa
+
+// esto es una declaración de una variable y asignación de un valor
+const DNI = 44728927;
+
+nombre = "Fede";
+apellido = "Ramos";
+
+// = representa asignación de datos
+
+provincia = "Buenos Aires"; //string
+
+provincia = "CABA";
+
+provincia = "Córdoba";
+
+provincia = 15; //int
+
+provincia = true; //boolean
+
+provincia = false;
+
+//funciones
+console.log(nombre + apellido + provincia );
+console.info(nombre + apellido + provincia );
+console.error(nombre + apellido + provincia );
+console.warn(nombre + apellido + provincia );
+
+
+//alert("Hola Mundo"); // ventana emergente
+
+
+
+// DNI = "PEPE"; // error en tiempo de ejecución
