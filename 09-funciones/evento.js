bd6f25c9bf047eed89dd8081b5499acaa615a544
Federico Ramos <federamos2203@gmail.com>
2024-08-03 13:18:47 -0300

    Funciones de tarea(hecho)

--- 09-funciones/evento.js
+++ 09-funciones/evento.js
@@ -11,8 +11,27 @@ function saludarAdmin() {
     console.log('Bienvenido Administrador a la sección de eventos');
 }
 
-//saludarAdmin(); 1:40
+//saludarAdmin();
 
 const titulo = () => {
-  alert('Hiciste click en el título de la página');
+    alert('Hiciste click en el título de la página');
 }
+
+// Tarea: Crear una pequeña calculadora con funciones, suma y resta
+
+// 1. Función parseInt me convierte el dato ingresado lo transforma a entero
+const sumar = () => {
+    let num1 = parseInt(prompt("Ingrese número a restar"));
+    let num2 = parseInt(prompt("Ingrese otro número a restar"));
+    let resultado = num1 + num2;
+    alert('Resultado de la suma: ' + resultado);
+}
+
+const restar = () => {
+    let num1 = parseInt(prompt("Ingrese número a restar"));
+    let num2 = parseInt(prompt("Ingrese otro número a restar"));
+    let resultado = num1 - num2;
+    alert('Resultado de la resta: ' + resultado);
+}
+
+
