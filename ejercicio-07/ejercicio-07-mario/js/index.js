f36c5dfd57b1d06101ff79e7f2d4399847ec1386
Federico Ramos <federamos2203@gmail.com>
2024-09-22 00:47:20 -0300

    Ejercicio07

--- ejercicio-07/ejercicio-07-mario/js/index.js
+++ ejercicio-07/ejercicio-07-mario/js/index.js
@@ -5,14 +5,12 @@ let span = document.getElementById("nombrePersonaje");
 
 let botonPresentar = document.getElementById("botonPresentar");
 
-let div = document.get
-
 botonPresentar.addEventListener('click', (e) => {
     e.preventDefault();
     const user = prompt("¿Quién se presenta hoy? (Mario, Luigi, Bowser, Peach, Yoshi, Toad, Toadette, Daisy)").toLowerCase();
     console.log(user);   
     mostrarPersonajes(user);
-    reset(user);
+    //botonPresentar.style.display = "none";
 });
 
 
@@ -55,55 +53,7 @@ const mostrarPersonajes = (user) => {
         default:
             span.innerHTML= "Desconocido";
             break;
-        //botonPresentar.style.display = "none";
-    };
+        };
 };
 
-const reset = (user) => {
-    let div = document.getElementById(`${user}`);
-    switch (user) {
-        case 'mario':
-            span.innerHTML = `${personajes[0]}`;
-            div.setAttribute("title", "Presentado");
-        break;
-        case 'luigi':
-            span.innerHTML = `${personajes[1]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'bowser':
-            span.innerHTML = `${personajes[2]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'peach':
-            span.innerHTML = `${personajes[3]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'yoshi':
-            span.innerHTML = `${personajes[4]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'toad':
-            span.innerHTML = `${personajes[5]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'toadette':
-            span.innerHTML = `${personajes[6]}`;
-            div.setAttribute("title", "");
-            break;
-        case 'daisy':
-            span.innerHTML = `${personajes[7]}`;
-            div.setAttribute("title", "");
-            break;
-        default :
-            div.getElementsByClassName("cerrar");
-            div.setAttribute("title", "");
-            break;
-    }; 
-}; 
-
-if (user == "mario") {
-    span.innerHTML = `${personajes[0]}`;
-    div.setAttribute("title", "Presentado");
-} else {
-    div.getElementsByTagName();
-}
+// Me gustaría saber como sería hecho con los bonus, que no los pude sacar 
